a65f43f0f2251b19f88e51454d099e730cd45d27
akbrijesh <akbrijesh@gmail.com>
2021-08-23 08:52:39 +0530

    arrow function

---
+++ arrow.js
@@ -0,0 +1,12 @@
+const studentList = students => console.log(students);
+
+studentList([
+    'Brijesh',
+    'Bonnie',
+    'Robert',
+    'Atul',
+    "Sandesh"
+]);
+
+let list = ['apple', 'banana', 'cherry'];
+['apple', 'banana', 'cherry','dragonfruit'].map(item=>    console.log(item));
